fecaff5de3543c8a692d5498761620682638d8ff
Gonzalo Piñeiro <gon.pineiro@gmail.com>
2019-10-29 20:00:14 -0300

    Manipulacion de Numeros 0.2

---
+++ Numeros.js
@@ -0,0 +1,19 @@
+var edad = 28;
+//edad = edad + 1;
+edad += 1;
+
+
+var peso = 75;
+//peso = peso - 2;
+
+var PrecioVino = 200.3
+var total2 = PrecioVino  * 3 ;
+
+var total = Math.round(PrecioVino * 100 * 3) / 100;
+
+var totalStr = total.toFixed(2);            // Cantidad de Decimales, en string
+var TotalFloat = parseFloat(totalStr);      // Transforma en Punto Flotante
+
+var pizza = 8;
+var persona = 2;
+var CantidadDePorcionesPorPersona = pizza / persona
