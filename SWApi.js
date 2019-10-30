5922d1cb770bbb867db45c94fd40ce4b85aa27e2
Gonzalo Piñeiro <gon.pineiro@gmail.com>
2019-10-30 19:05:19 -0300

    CallBacks

--- SWApi.js
+++ SWApi.js
@@ -4,15 +4,42 @@ const PEOPLE_URL = 'people/:id'
 
 const opts =  { crossDomain: true }
 
-const onPersoResponse = function(personaje){
-    console.log(personaje.name)
-}
 
-function obtenerPersonaje(id){
+function obtenerPersonaje(id, callback){
     const url = `${ API_URL }${PEOPLE_URL.replace(':id', id)}`
-    $.get(url, opts, onPersoResponse)
+
+    $
+        .get(url, opts, callback)
+        .fail(() => {console.log(id)})
+        
 }
 
-for (var i = 0; i < 4; i++) {
-    obtenerPersonaje(i+1)    //EJEMPLO DE ASINCRONISMO
-}
+obtenerPersonaje(1, function (personaje) {
+    console.log(personaje.name)
+    obtenerPersonaje(2, function (personaje) {
+        console.log(personaje.name)
+        obtenerPersonaje(3, function (personaje) {
+            console.log(personaje.name)
+            obtenerPersonaje(4,function (personaje) {
+                console.log(personaje.name)
+                obtenerPersonaje(5,function (personaje) {
+                    console.log(personaje.name)
+                    obtenerPersonaje(6,function (personaje) {
+                        console.log(personaje.name)
+                        obtenerPersonaje(7,function (personaje) {
+                            console.log(personaje.name)
+                            obtenerPersonaje(8,function (personaje) {
+                                console.log(personaje.name)
+                                obtenerPersonaje(9,function (personaje) {
+                                    console.log(personaje.name)
+                                    obtenerPersonaje(10)
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
+       
